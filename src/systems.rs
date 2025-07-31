7b7581cf05766d323a074517d9a5cdadfcfd82d9
5CYTH3 <ScytheD@outlook.fr>
2025-07-31 16:20:42 +0200

    New sh?

--- src/systems.rs
+++ src/systems.rs
@@ -1,4 +1,7 @@
-use bevy::{prelude::{Commands, Query, With, Camera2dBundle, Transform, Without, Component}, window::{Window, PrimaryWindow}};
+use bevy::{
+    prelude::{Camera2dBundle, Commands, Component, Query, Transform, With, Without},
+    window::{PrimaryWindow, Window},
+};
 
 use crate::game::player::Player;
 
@@ -6,29 +9,27 @@ use crate::game::player::Player;
 pub struct PlayerCamera;
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    let window = window_query.get_single().unwrap(); 
+    let window = window_query.get_single().unwrap();
 
     let scale = 0.4;
 
     let mut camera = Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0), 
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..Default::default()
     };
 
     camera.projection.scale = scale;
     camera.projection.far = 1000.0 / scale;
 
-    commands.spawn(
-        (
-            camera,       
-            PlayerCamera,
-        )
-    );
+    commands.spawn((camera, PlayerCamera));
 }
 
-pub fn followup_camera(p_query: Query<&Transform, (With<Player>, Without<PlayerCamera>)>, mut c_query: Query<&mut Transform, (With<PlayerCamera>, Without<Player>)>) { 
+pub fn followup_camera(
+    p_query: Query<&Transform, (With<Player>, Without<PlayerCamera>)>,
+    mut c_query: Query<&mut Transform, (With<PlayerCamera>, Without<Player>)>,
+) {
     let p_transform = p_query.get_single().unwrap();
-    let mut _c_transform = c_query.get_single_mut().unwrap(); 
-    
+    let mut _c_transform = c_query.get_single_mut().unwrap();
+
     _c_transform.translation = p_transform.translation
 }
